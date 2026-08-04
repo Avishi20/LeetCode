50cfa1e6830248d2a751a7e4238bc04f924c89b4
Avishi20 <avishinagar01@gmail.com>
2026-08-04 19:58:39 +0530

    Time: 0 ms (100%), Space: 47.1 MB (82.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,8 +3,9 @@ class Solution {
         int n = nums.length;
         int sum = n*(n+1)/2;
         int actualsum = 0;
-        for(int i = 0; i < nums.length; i++ ){
-            actualsum += nums[i];
+
+        for(int num : nums ){
+            actualsum += num;
         }
         return sum - actualsum;
 
